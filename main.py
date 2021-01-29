7ba248132a1aab222a815fb8fd291950af4279ba
varoozhan <varoozhan@gmail.com>
2021-01-28 21:30:27 -0800

    Fixed the for loops, made the code cleaner

--- main.py
+++ main.py
@@ -3,41 +3,34 @@ from cpe import CPE
 
 
 def listOfCpes(userInput, NvdNistDBJson):
-    # We should be able to do this with only one for loop. This is just for me to make it work first and learn some Python
-    list_of_cpe_s = []
-    for nvdobject in NvdNistDBJson:
-        if not nvdobject['configurations']['nodes']:
-            print('list empty')
-        else:
-            list_of_cpe_s.append(nvdobject['configurations']['nodes'])
-    # if(NvdNistDBJson[30]['configurations']['nodes'][2]['cpe_match'] != n)
-
     # list_of_cpes = NvdNistDBJson[30]['configurations']['nodes'][2]['cpe_match']
     # print(list_of_cpes[0]['cpe23Uri'])
     # Extracting cpes only and returning them as a list
     cpe_s = []
     count = 1
-
-    for cpe in list_of_cpe_s:
-        for pos in cpe:
-            # print('All')
-            # print(pos)
-            if pos['operator'] == 'OR':
-                for cpe23Uri in pos['cpe_match']:
-                    print(cpe23Uri['cpe23Uri'])
-                # print(pos['cpe_match'])
-            else:
-                if not pos.get('children'):
-                    print('and without children')
-                    print(pos['cpe_match'])
+    for nvdobject in NvdNistDBJson:
+        if not nvdobject['configurations']['nodes']:
+            print('list empty')
+        else:
+            # print(type(nvdobject['configurations']['nodes']))
+            for cpe in nvdobject['configurations']['nodes']:
+                # print(cpe)
+                if cpe['operator'] == 'OR':
+                    for cpe23Uri in cpe['cpe_match']:
+                        print(cpe23Uri['cpe23Uri'])
+                            # print(pos['cpe_match'])
                 else:
-                    print('and with children')
-                    for child in pos['children']:
-                        for cpe23Uri in child['cpe_match']:
-                            print(cpe23Uri['cpe23Uri'])
-                            # print(type(child['cpe_match']))
+                    if not cpe.get('children'):
+                        print('and without children')
+                        print(cpe['cpe_match'])
+                    else:
+                        print('and with children')
+                        for child in cpe['children']:
+                            for cpe23Uri in child['cpe_match']:
+                                print(cpe23Uri['cpe23Uri'])
+                                    # print(type(child['cpe_match']))
         # print(cpe)
-
+    print("************" + userInput)
         # for pos in cpe:
             # if not pos['cpe_match']:
             #     print('no matches')
